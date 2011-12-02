fff5eea7a48cf29628c03d74bc8a021a4cbaab72
Erik Allik <eallik@gmail.com>
2011-12-02 16:33:33 +0200

    ASD

--- el-get/.loaddefs.el
+++
@@ -1,28 +0,0 @@
-;;; .loaddefs.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (smex-initialize) "smex" "smex/smex.el" (20169
-;;;;;;  529))
-;;; Generated autoloads from smex/smex.el
-
-(autoload 'smex-initialize "smex" "\
-Not documented
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("el-get/el-get-install.el" "el-get/el-get.el")
-;;;;;;  (20169 529 948821))
-
-;;;***
-
-(provide '.loaddefs)
-;; Local Variables:
-;; version-control: never
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; .loaddefs.el ends here
